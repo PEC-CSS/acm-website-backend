2d61f53e3954260c109396b688e0f83634a7e4ca
Harshpreet Singh Johar <hpsj3112@gmail.com>
2023-06-11 23:32:02 +0530

    Starting project

---
+++ src/main/java/com/pecacm/backend/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package com.pecacm.backend.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    @ResponseStatus(HttpStatus.OK)
+    public String health() {
+        return "Health OK";
+    }
+}
