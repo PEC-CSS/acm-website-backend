00db7ce41bf968176bb0b35468a95b5f13f55d4e
notSanil <44319106+notSanil@users.noreply.github.com>
2023-07-20 23:35:36 +0530

    Added role hierarchy to constants

--- src/main/java/com/pecacm/backend/constants/Constants.java
+++ src/main/java/com/pecacm/backend/constants/Constants.java
@@ -5,10 +5,16 @@ public class Constants {
 
     /* Authorization Constants */
     //TODO: This is an ugly solution, need to refactor this to use values from the enum somehow
+    public static final String OR = " or ";
     public static final String HAS_ROLE_ADMIN = "hasRole('Admin')";
     public static final String HAS_ROLE_CORE = "hasRole('Core')";
     public static final String HAS_ROLE_EB = "hasRole('ExecutiveBody')";
     public static final String HAS_ROLE_IB = "hasRole('ImplementationBody')";
     public static final String HAS_ROLE_MEMBER = "hasRole('Member')";
-
+    public static final String HAS_ROLE_ANONYMOUS = "hasRole('ANONYMOUS')";
+    public static final String HAS_ROLE_CORE_AND_ABOVE = HAS_ROLE_ADMIN + OR + HAS_ROLE_CORE;
+    public static final String HAS_ROLE_EB_AND_ABOVE = HAS_ROLE_CORE_AND_ABOVE + OR + HAS_ROLE_EB;
+    public static final String HAS_ROLE_IB_AND_ABOVE = HAS_ROLE_EB_AND_ABOVE + OR + HAS_ROLE_IB;
+    public static final String HAS_ROLE_MEMBER_AND_ABOVE = HAS_ROLE_IB_AND_ABOVE + OR + HAS_ROLE_MEMBER;
+    public static final String HAS_ANY_ROLE = HAS_ROLE_MEMBER_AND_ABOVE + OR + HAS_ROLE_ANONYMOUS;
 }
