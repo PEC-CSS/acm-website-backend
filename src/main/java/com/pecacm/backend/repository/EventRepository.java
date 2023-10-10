0f0281e9423833965d543bc36a1af3f0552ebf22
Harasees Singh <haraseessingh@Haraseess-MacBook-Pro.local>
2023-10-10 21:47:15 +0530

    added api to retrieve events between 2 respective dates

--- src/main/java/com/pecacm/backend/repository/EventRepository.java
+++ src/main/java/com/pecacm/backend/repository/EventRepository.java
@@ -1,20 +1,19 @@
 package com.pecacm.backend.repository;
 
 import com.pecacm.backend.entities.Event;
+import com.pecacm.backend.enums.Branch;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
-    List<Event> findByBranch(String branch);
-
+    List<Event> findByBranch(Branch branch);
     List<Event> findAllByEndedFalse();
-
     @Query("SELECT e from Event e WHERE e.startDate > :currDateTime ORDER BY e.startDate ASC LIMIT 1")
     Event getNearestEvent(@Param("currDateTime") LocalDateTime currDate);
+    List<Event> findAllByStartDateGreaterThanEqualAndEndDateLessThanEqual(LocalDateTime startDate, LocalDateTime endDate);
 }
