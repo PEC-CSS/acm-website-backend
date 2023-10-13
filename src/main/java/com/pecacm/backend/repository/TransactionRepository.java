72a66c6c8dee5894de0c641cb757425fd04a751a
Nanak <nanakjotsachdeva@gmail.com>
2023-10-13 21:06:54 +0530

    Added pagination to user events API and SupportController for other issues

--- src/main/java/com/pecacm/backend/repository/TransactionRepository.java
+++ src/main/java/com/pecacm/backend/repository/TransactionRepository.java
@@ -2,13 +2,13 @@ package com.pecacm.backend.repository;
 
 import com.pecacm.backend.entities.Transaction;
 import com.pecacm.backend.enums.EventRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    List<Transaction> findByUserId(Integer userId);
-    List<Transaction> findByUserIdAndRole(Integer userId, EventRole role);
+    Page<Transaction> findByUserId(Integer userId, PageRequest pageRequest);
+    Page<Transaction> findByUserIdAndRole(Integer userId, EventRole role, PageRequest pageRequest);
 }
