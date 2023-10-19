1b15a5a5650398b5f84c049d027e9157b57f781b
13jksingh <jaskeerat56.93@gmail.com>
2023-10-19 13:02:51 +0530

    Made event controller get reqs pageable

--- src/main/java/com/pecacm/backend/repository/EventRepository.java
+++ src/main/java/com/pecacm/backend/repository/EventRepository.java
@@ -2,6 +2,7 @@ package com.pecacm.backend.repository;
 
 import com.pecacm.backend.entities.Event;
 import com.pecacm.backend.enums.Branch;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +12,9 @@ import java.util.List;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Integer> {
-    List<Event> findByBranch(Branch branch);
-    List<Event> findAllByEndedFalse();
+    List<Event> findByBranch(Branch branch,PageRequest pageRequest);
+    List<Event> findAllByEndedFalse(PageRequest pageRequest);
     @Query("SELECT e from Event e WHERE e.startDate > :currDateTime ORDER BY e.startDate ASC LIMIT 1")
     Event getNearestEvent(@Param("currDateTime") LocalDateTime currDate);
-    List<Event> findAllByStartDateGreaterThanEqualAndEndDateLessThanEqual(LocalDateTime startDate, LocalDateTime endDate);
+    List<Event> findAllByStartDateGreaterThanEqualAndEndDateLessThanEqual(LocalDateTime startDate, LocalDateTime endDate, PageRequest pageRequest);
 }
