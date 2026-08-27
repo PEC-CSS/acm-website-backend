1741eb00eeaa9545deac1eb22fad8edbaf25ef8d
Kanavpreet-Singh <145096601+Kanavpreet-Singh@users.noreply.github.com>
2026-08-27 07:50:04 +0530

    refactor: remove API route version prefixes (#133)

--- src/main/java/com/pecacm/backend/controllers/EmailController.java
+++ src/main/java/com/pecacm/backend/controllers/EmailController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/v1/email")
+@RequestMapping("/email")
 public class EmailController {
     private final EmailService emailService;
     private final UserService userService;
